9e4398dc450a734309c4780dcb7fe90aeced2cc0
huanshenyi <txy1226052>
2019-04-10 11:26:12 +0900

    qoraiche/laravel-mail-editor

---
+++ resources/views/mails/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<p><strong>hello world</strong></p>
+<p>test mail</p>
+</body>
+</html>
